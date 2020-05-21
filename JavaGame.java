a5da439b6e91cbd5f37654b727afced05ae3695a
Automatus <57252435+Automatus@users.noreply.github.com>
2020-05-21 16:06:04 +0200

    Update JavaGame.java

--- JavaGame.java
+++ JavaGame.java
@@ -11,6 +11,7 @@ public class Game {
         String player1;
         String player2 = "Computer";
         String symbol = "X";
+        int counter = 0;
         Scanner usrinput = new Scanner(System.in);
 
         System.out.println("\n Boter Kaas en eieren  ---  The GAME \n");
@@ -25,11 +26,20 @@ public class Game {
 
         while (gaming){
             printTable(table);
-            table = getCoordinates(player1, table, symbol);
-            symbol = "O";
-            printTable(table);
-            table = getCoordinates(player2, table, symbol);
+            gaming = checkWinner(table, symbol, player2);
             symbol = "X";
+            table = getCoordinates(player1, table, symbol);
+            counter++;
+            if (counter == 5){
+                System.out.println("You are both winners! :)");
+                gaming = false;
+            }
+            printTable(table);
+            gaming = checkWinner(table, symbol, player1);
+            symbol = "O";
+            table = getCoordinates(player2, table, symbol);
+
+
         }
     }
 
@@ -54,9 +64,21 @@ public class Game {
         }
         return table;
     }
-    public static boolean checkWinner(String[][] table){
-        
-    boolean gaming = true;
-    return gaming;
+
+    public static boolean checkWinner(String[][] table, String symbol, String player){
+        boolean gaming = true;
+        if ((table[0][0].equals(symbol) && table[0][1].equals(symbol) && table[0][2].equals(symbol)) ||
+        (table[1][0].equals(symbol) && table[1][1].equals(symbol) && table[1][2].equals(symbol)) ||
+        (table[2][0].equals(symbol) && table[2][1].equals(symbol) && table[2][2].equals(symbol)) ||
+        (table[0][0].equals(symbol) && table[1][0].equals(symbol) && table[2][0].equals(symbol)) ||
+        (table[1][0].equals(symbol) && table[1][1].equals(symbol) && table[1][2].equals(symbol)) ||
+        (table[2][0].equals(symbol) && table[2][1].equals(symbol) && table[2][2].equals(symbol)) ||
+        (table[0][0].equals(symbol) && table[1][1].equals(symbol) && table[2][2].equals(symbol)) ||
+        (table[2][0].equals(symbol) && table[1][1].equals(symbol) && table[0][2].equals(symbol))){
+            System.out.println(player + "has won!");
+            gaming = false;
+        }
+
+        return gaming;
     }
 }
